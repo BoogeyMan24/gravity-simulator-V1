5bca6245c36f45f811b71ef6b93b6f07e2020d9f
BoogeyMan24 <peta.hora11@gmail.com>
2023-07-18 14:35:57 -0400

    READMEandComments

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gravity Simulator
+
+## Create planets with masses and see how they interact with each other
+
+### Try to make a planet orbit around another planet/star
+
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,16 +4,23 @@
 
 // Add Physics
 
-
+// mass - changes how much other objects effect it (larger mass means less movement)
+// radius - visual only
+// position - starting position
+// color - visual only (I would recommend making each circle a different color)
 
 int main(int argc, char* argv[]) {
-    Application app(std::string("Epic Physics Engine"));
+    Application app(std::string("Epic Gravity Simulator"));
 
-
+	//											mass  radius  starting position   color of object in rgba
 	Entity planet(&app.entity_manager.m_entities, 100000, 50, Vector2(400, 300), Color(255, 0, 0, 255));
 	Entity planet1(&app.entity_manager.m_entities, 5, 5, Vector2(600, 300), Color(0, 255, 0, 255));
+
+	// add planets to entity_manager so they update/draw every frame
 	app.entity_manager.add(&planet1);
 	app.entity_manager.add(&planet);
+
+	// edit starting velocity
 	planet1.velocity = Vector2(0, 400);
 
     app.loop();
